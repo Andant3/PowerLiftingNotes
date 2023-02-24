f90fd7df891e7eac6d1d47f3be6ab7d6bb428bb2
Andant3 <kamikadze3571@gmail.com>
2023-02-24 12:23:09 +0100

    Setting up all LiveData and complete ViewModel

--- app/src/main/java/com/example/powerliftingnotes/presentation/ExerciseViewModel.kt
+++ app/src/main/java/com/example/powerliftingnotes/presentation/ExerciseViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.powerliftingnotes.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.powerliftingnotes.data.ExerciseListRepositoryImpl
 import com.example.powerliftingnotes.domain.AddExerciseUseCase
@@ -7,77 +9,122 @@ import com.example.powerliftingnotes.domain.EditExerciseUseCase
 import com.example.powerliftingnotes.domain.Exercise
 import com.example.powerliftingnotes.domain.GetExerciseUseCase
 
-class ExerciseViewModel: ViewModel() {
+class ExerciseViewModel : ViewModel() {
 
     //Incorrect way for repository realisation
     //(Presentation and Data layers dependency)
     private val repository = ExerciseListRepositoryImpl
 
+    private val _exercise = MutableLiveData<Exercise>()
+    val exercise: LiveData<Exercise>
+        get() = _exercise
+
+    private val _shouldCloseScreen = MutableLiveData<Unit>()
+    val shouldCloseScreen: LiveData<Unit>
+        get() = _shouldCloseScreen
+
+
+    //Method to appear LiveData variable with access to values for ViewModel
+    //and no access for View
+    private val _errorInputName = MutableLiveData<Boolean>()
+    val errorInputName: LiveData<Boolean>
+        get() = _errorInputName
+
+    private val _errorInputWeight = MutableLiveData<Boolean>()
+    val errorInputWeight: LiveData<Boolean>
+        get() = _errorInputName
+
+    private val _errorInputReps = MutableLiveData<Boolean>()
+    val errorInputReps: LiveData<Boolean>
+        get() = _errorInputName
+
     private val getExerciseUseCase = GetExerciseUseCase(repository)
     private val addExerciseUseCase = AddExerciseUseCase(repository)
     private val editExerciseUseCase = EditExerciseUseCase(repository)
 
-    fun getExercise(id: Int){
-        val exercise = getExerciseUseCase.getExerciseById(id)
+    fun getExercise(exerciseId: Int) {
+        val exercise = getExerciseUseCase.getExerciseById(exerciseId)
+        _exercise.value = exercise
     }
 
-    fun addExercise(inputName: String?, inputWeight: String?, inputReps: String?){
+    fun addExercise(inputName: String?, inputWeight: String?, inputReps: String?) {
         val name = parseName(inputName)
         val weight = parseWeight(inputWeight)
         val reps = parseReps(inputReps)
         val fieldsValid = validateInput(name, weight, reps)
-        if (fieldsValid){
+        if (fieldsValid) {
             val exercise = Exercise(name, weight, reps, true)
             addExerciseUseCase.addExercise(exercise)
+            finishWork()
         }
+
     }
 
-    fun editExercise(inputName: String?, inputWeight: String?, inputReps: String?){
+    fun editExercise(inputName: String?, inputWeight: String?, inputReps: String?) {
         val name = parseName(inputName)
         val weight = parseWeight(inputWeight)
         val reps = parseReps(inputReps)
         val fieldsValid = validateInput(name, weight, reps)
-        if (fieldsValid){
-            val exercise = Exercise(name, weight, reps, true)
-            editExerciseUseCase.editExercise(exercise)
+        if (fieldsValid) {
+            _exercise.value?.let {
+                val exercise = it.copy(name = name, weight = weight, reps = reps)
+                editExerciseUseCase.editExercise(exercise)
+                finishWork()
+            }
         }
     }
 
-    private fun parseName(inputName: String?): String{
+    private fun parseName(inputName: String?): String {
         return inputName?.trim() ?: ""
     }
 
-    private fun parseWeight(inputWeight: String?): Int{
+    private fun parseWeight(inputWeight: String?): Int {
         return try {
             inputWeight?.trim()?.toInt() ?: 0
-        }catch (e: NullPointerException){
+        } catch (e: NullPointerException) {
             0
         }
     }
 
-    private fun parseReps(inputReps: String?): Int{
+    private fun parseReps(inputReps: String?): Int {
         return try {
             inputReps?.trim()?.toInt() ?: 0
-        }catch (e: NullPointerException){
+        } catch (e: NullPointerException) {
             0
         }
     }
 
-    private fun validateInput(name: String, weight: Int, reps:Int): Boolean{
+    private fun validateInput(name: String, weight: Int, reps: Int): Boolean {
         var result = true
 
-        if(name.isBlank()){
-            // TODO: Show error input name
+        if (name.isBlank()) {
+            _errorInputName.value = true
             result = false
         }
-        if(weight <= 0){
-            // TODO: Show error input weight
+        if (weight <= 0) {
+            _errorInputWeight.value = true
             result = false
         }
-        if(reps <= 0){
-            // TODO: Show error input reps
+        if (reps <= 0) {
+            _errorInputReps.value = true
             result = false
         }
         return result
+    }
+
+    private fun resetErrorInputName() {
+        _errorInputName.value = false
+    }
+
+    private fun resetErrorInputWeight() {
+        _errorInputWeight.value = false
+    }
+
+    private fun resetErrorInputReps() {
+        _errorInputReps.value = false
+    }
+
+    private fun finishWork() {
+        _shouldCloseScreen.value = Unit
     }
 }
